2c109918ba780c5de1bf34c53151d14943f3046e
José Augusto <104844662+AugustoOliveira099@users.noreply.github.com>
2023-10-09 19:08:50 +0000

    Update the script

--- Python_Essentials_for_MLOps/Project_1/functions.py
+++ Python_Essentials_for_MLOps/Project_1/functions.py
@@ -1,9 +1,9 @@
 """"
-Scrypt destinado às funções que serão utilizadas na
-main - movie_recommendation_system.
+Script destinado às funções que serão utilizadas na
+main: ./movie_recommendation_system.py.
 """
 
-# import libraries
+# Importa bibliotecas
 import os
 import re
 import logging
@@ -11,14 +11,15 @@ import zipfile
 import requests
 import numpy as np
 import pandas as pd
-from tqdm import tqdm 
+import tqdm
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Configuração inicial do logging
+# Com level logging.INFO, também é englobado o level logging.ERROR
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-def clean_title(title: str) -> str:
+def clean_movie_title(title: str) -> str:
     """
     Função que faz com que uma string possua apenas letras, números e 
     espaços
@@ -29,10 +30,10 @@ def clean_title(title: str) -> str:
     Return:
         cleaned_title (str): Título apenas com letras, números e espaços
     """
-    cleaned_title = re.sub(r"[^a-zA-Z0-9\s]", "", title)
-    return cleaned_title
+    clean_title = re.sub(r"[^a-zA-Z0-9\s]", "", title)
+    return clean_title
 
-def search_similar_movies(movies_df: pd.DataFrame, 
+def search_similar_movies_by_title(movies_df: pd.DataFrame, 
                           vectorizer: TfidfVectorizer,
                           title: str) -> pd.DataFrame:
     """
@@ -43,10 +44,108 @@ def search_similar_movies(movies_df: pd.DataFrame,
         vectorizer (TfidfVectorizer): Instância da classe TfidfVectorizer
         title (str): Título do filme
     """
-    cleaned_title = clean_title(title)
-    query_vec = vectorizer.transform([cleaned_title])
+    clean_title = clean_movie_title(title)
+    query_vec = vectorizer.transform([clean_title])
     similarity = cosine_similarity(query_vec, 
                                    vectorizer.fit_transform(movies_df["clean_title"])).flatten()
     indices = np.argpartition(similarity, -5)[-5:]
     results = movies_df.iloc[indices].iloc[::-1]
     return results
+
+def find_similar_movies(clean_title: str,
+                        movies_df: pd.DataFrame,
+                        ratings_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Encontra 10 filmes recomendados com base no nome do filme, baseado em usuários
+    com gostos similares e com usuários no geral
+
+    Args:
+        clean_title (str): Título do filme que será analisado
+        movies_df (pd.DataFrame): Dataframe contendo todos os filmes
+        ratings_df (pd.DataFrame): Dataframe com a classificação de usuários para os filmes
+
+    Return:
+        rec_percentages (pd.DataFrame): Os 10 filmes recomendados
+    """
+    # Obtém o id do filme
+    movie_id = movies_df[movies_df["clean_title"] == clean_title]["movieId"].values[0]
+
+    # Encontra os usuários que deram uma nota maior que 4 para o filme informado
+    similar_users = ratings_df[(ratings_df["movieId"] == movie_id) and \
+                               (ratings_df["rating"] > 4.5)]["userId"].unique()
+
+    # Encontra as ids dos filmes cujas classiciações dos usuários "similar_users"
+    # foram maior que 4.5 (os ids dos filmes podem se repetir). Ou seja, os filmes
+    # que são recomendados para nós por aquele que também gostaram do mesmo filme
+    # que a gente
+    similar_user_recs = ratings_df[(ratings_df["userId"].isin(similar_users)) and \
+                                   (ratings_df["rating"] > 4.5)]["movieId"]
+
+    # Calcula a porcentagem das recomendações
+    similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
+
+    # Filtra as recomendações de filmes para que apenas aquelas que tiveram
+    # uma recorrência maior que 10% estejam presentes
+    similar_user_recs = similar_user_recs[similar_user_recs > .10]
+
+    # Todos os usários que classificaram os filmes recomendados com uma nota maior que 4.5
+    all_users = ratings_df[(ratings_df["movieId"].isin(similar_user_recs.index)) & \
+                           (ratings_df["rating"] > 4.5)]
+
+    # Porcentagem de usuários que também classificaram os filmes recomendados
+    # com uma nota maior que 4.5
+    all_users_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
+
+    # Junta as duas séries em um dataframe
+    rec_percentages = pd.concat([similar_user_recs, all_users_recs], axis=1)
+
+    # Renomeia as colunas
+    rec_percentages.columns = ["similar", "all"]
+
+    # Obtém o score
+    rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+
+    # Ordena os scores em ordem descrescente
+    rec_percentages = rec_percentages.sort_values("score", ascending=False)
+
+    # Retira o filme passado como parâmetro do dataframe, uma vez que não faz
+    # sentido indicar o filmes pelo qual a pessoa está procurando
+    rec_percentages = rec_percentages[rec_percentages.index != movie_id]
+
+    return rec_percentages.head(10).merge(movies_df, \
+                                          left_index=True, \
+                                          right_on="movieId")[["score", "title", "genres"]]
+
+def download_zip_file() -> None:
+    """
+    Faz download dos arquivos necessários para que o script funcione corretamente
+    """
+    # URL do arquivo
+    url="http://files.grouplens.org/datasets/movielens/ml-25m.zip"
+
+    # Nome do arquivo zip a ser baixado
+    zip_filename = "ml-25m.zip"
+
+    with requests.Session() as session:
+        response = session.get(url, stream=True)
+        total_size = int(response.headers.get('content-length', 0))
+
+        # Definindo o chunk size
+        chunk_size = 128 * 1024
+
+        # Download do arquivo
+        with open(zip_filename, 'wb') as file:
+            for data in tqdm.tqdm(response.iter_content(chunk_size=chunk_size),
+                            total=total_size // chunk_size,
+                            unit='KB',
+                            desc=zip_filename,
+                            leave=True):
+                file.write(data)
+
+    # Descompacta os arquivos
+    logging.info("Unzipping the file")
+    with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
+        zip_ref.extractall(".")
+
+    # Remove o arquivo ZIP
+    os.remove(zip_filename)
--- Python_Essentials_for_MLOps/Project_1/movie_recommendation_system.py
+++ Python_Essentials_for_MLOps/Project_1/movie_recommendation_system.py
@@ -1,11 +1,86 @@
 """"
-De um modo geral, esse scrypt indica 10 filmes baseados
-no nome de um filme passado como parâmetro. Ele leva em
-conta uma lista de filmes, que faremos download, e faz 
-uso de processamento de texto.
+De um modo geral, este script indica 10 filmes baseados
+no nome de um filme passado como parâmetro e na classificação
+dos filmes pelos usuários
 """
 # import libraries
+import os
+import zipfile
+import argparse
+import logging
+import requests
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# https://files.grouplens.org/datasets/movielens/ml-25m.zip
-movies = pd.read_csv("movies.csv")
+from functions import clean_movie_title, \
+                      search_similar_movies_by_title, \
+                      find_similar_movies, \
+                      download_zip_file
+
+# Verifica se o script está sendo executado como programa principal
+if __name__ == '__main__':
+    # Configuração inicial do logging
+    # Com level logging.INFO, também é englobado o level logging.ERROR
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+    # Iniciando o parse para receber o título do filme como parâmetro
+    parser = argparse.ArgumentParser(description="Movie Recommendation System")
+    parser.add_argument("-title", type=str, help="The title of the movie")
+    args = parser.parse_args()
+
+    # get the movie title
+    movie_title = args.title
+
+    # read the data
+    try:
+        if not os.path.exists("ml-25m"):
+            logging.info("Downloading the zip file")
+
+            download_zip_file()
+        else:
+            logging.info("The data is already downloaded")
+    except requests.exceptions.ConnectionError:
+        logging.error("Connection Error")
+    except requests.exceptions.Timeout:
+        logging.error("Timeout Error")
+    except requests.exceptions.HTTPError:
+        logging.error("HTTP Error")
+    except zipfile.BadZipFile:
+        logging.error("The downloaded file is not a valid ZIP file.")
+
+    # Importando o arquivo
+    logging.info("Getting the data")
+    movies_df = pd.read_csv("ml-25m/movies.csv")
+
+    # Limpando o título do filme
+    logging.info("Cleaning the movie title")
+    movies_df["clean_title"] = movies_df["title"].apply(clean_movie_title)
+
+    # Instanciando o TF-IDF vectorizer
+    logging.info("Instantiating the TF-IDF vectorizer")
+    tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2))
+
+    # Ajustando e transformando os dados com base na instância TF-IDF
+    logging.info("Ajustando e transformando os dados")
+    tfidf_matrix = tfidf_vectorizer.fit_transform(movies_df["clean_title"])
+
+    # Obtém os 5 filmes mais similares a partir do título
+    results = search_similar_movies_by_title(movies_df, tfidf_vectorizer, movie_title)
+
+    # Lendo os dados de classificação dos filmes
+    ratings_df = pd.read_csv("ml-25m/ratings.csv")
+
+    logging.info("Finding the similar movies")
+
+    movies_recommendations = find_similar_movies(movies_df, ratings_df, results.iloc[0]["title_clean"])
+
+    # Exibe as recomendações
+    if movies_recommendations.empty:
+        print(f"No recommendations could be found based on the movie {movie_title}.")
+        print("Please, check if there are no spelling errors in the provided title.")
+        print("If the error persists, change the movie title.")
+    else:
+        print(f"Our 10 recommendations for you, based on the movie {movie_title}, are:")
+        for index, row in movies_recommendations.iterrows():
+            print(row["title"], row["genres"])
+            print("")
